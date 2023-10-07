235fb718cf538d7750be8537c69452c7227a6255
tiendung <mtdung1997@gmail.com>
2023-10-07 20:05:02 +0700

    Update serverless.yaml file and commented handler.js file.

--- starter/backend/handler.js
+++ starter/backend/handler.js
@@ -1,42 +1,42 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+// import { DynamoDB } from "@aws-sdk/client-dynamodb";
+// import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
-const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
-const groupsTable = process.env.GROUPS_TABLE;
+// const dynamoDbClient = DynamoDBDocument.from(new DynamoDB());
+// const groupsTable = process.env.GROUPS_TABLE;
 
-const data = [
-  [
-    { id: '1', name: 'Dogs', description: 'Only dog images here!' },
-    {
-      id: '2',
-      name: 'Nature',
-      description: 'What can be a better object for photography'
-    },
-    {
-      id: '3',
-      name: 'Cities',
-      description: 'Creative display of urban settings'
-    }
-  ]
-]
+// const data = [
+//   [
+//     { id: '1', name: 'Dogs', description: 'Only dog images here!' },
+//     {
+//       id: '2',
+//       name: 'Nature',
+//       description: 'What can be a better object for photography'
+//     },
+//     {
+//       id: '3',
+//       name: 'Cities',
+//       description: 'Creative display of urban settings'
+//     }
+//   ]
+// ]
 
-export async function hello(event) {
-  console.log('Processing event: ', event);
+// export async function hello(event) {
+//   console.log('Processing event: ', event);
 
-  const result = await dynamoDbClient.scan({
-    TableName: groupsTable,
-    Limit: 20
-  })
+//   const result = await dynamoDbClient.scan({
+//     TableName: groupsTable,
+//     Limit: 20
+//   })
 
-  const items = result.Items
+//   const items = result.Items
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items: items
-    })
-  }
-}
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     },
+//     body: JSON.stringify({
+//       items: items
+//     })
+//   }
+// }
