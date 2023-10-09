84852aebe1eb57cc4f2631b301856ba4f8cb099e
tiendung <mtdung1997@gmail.com>
2023-10-10 01:33:42 +0700

    Implemented the create to do handle and associated functions.

--- starter/client/src/index.js
+++ starter/client/src/index.js
@@ -7,6 +7,7 @@ import './index.css'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
